8e4cf69ae60e14612e6a1d7c0d3737898ce4338b
Stefan Hadamek <stefan.hadamek@fau.de>
2023-11-06 21:34:11 +0100

    renamed functions and variables and changed logic of main method

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,79 @@
 import random
 
 
-def function_A(min, max):
+def getRandomIntInIntervall( min: int , max: int):
     """
-    Random integer.
+    function returns random Integer in Intervall [min,max]
+    
+    Args:
+        min (int): minimum of interval
+        max (int): maximum of interval
+    
+    Returns: 
+        function returns random Integer in Intervall [min,max]
     """
+    # returns the output using the random lib
     return random.randint(min, max)
 
 
-def function_B():
+def chooseRandomMathOperator():
+    """
+    Returns:
+        function returns random Operator of Array ['+', '-', '*']
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def performOperation( first_op: int, second_op: int, op):
+    """
+    function performs an mathematical Operation
+    
+    Args:
+        first_op (int): First Operand
+        second_op (int): Second Operand
+        op (char): operator -> [+,-,*]
+        
+    Returns: 
+        function performs the mathematical operator -> "first_op op second_op"
+    """
+    # creating string to show full operation
+    statement = f"{first_op} {op} {second_op}"
+    # deciding the operation with value of op.
+    if op == '+': out = first_op + second_op
+    elif op == '-': out = first_op - second_op
+    else: out = first_op * second_op
+    return statement, out
 
 def math_quiz():
     s = 0
-    t_q = 3.14159265359
+    #t_q = 3.14159265359
+    t_qnew =3
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    # go for three rounds
+    for _ in range(t_qnew):
+        first_op = getRandomIntInIntervall(1, 10) 
+        second_op = getRandomIntInIntervall(1, 5) 
+        op = chooseRandomMathOperator()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+        PROBLEM, ANSWER = performOperation(first_op=first_op, second_op=second_op, op=op)
+        try:
+            print(f"\nQuestion: {PROBLEM}")
+            useranswer = input("Your answer: ")
+            useranswer = int(useranswer)
 
-        if useranswer == ANSWER:
-            print("Correct! You earned a point.")
-            s += -(-1)
-        else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
-
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+            if useranswer == ANSWER:
+                print("Correct! You earned a point.")
+                s += 1
+            else:
+                print(f"Wrong answer. The correct answer is {ANSWER}.")
+        except:
+            print(f" The Answer wasn't a number! This operation will not be counted")
+            t_qnew -= 1
+    percentage = (s*100)/t_qnew
+    print(f"\nGame over! Your score is: {percentage}%")
 
 if __name__ == "__main__":
     math_quiz()
